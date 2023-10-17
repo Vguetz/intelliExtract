9c997811fb11a3ecd8a3dbba4974468d686b5f21
Vguetz <gomezlucas712@gmail.com>
2023-10-17 16:32:08 -0300

    feature:stripe-webhook

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import { buttonVariants } from "./ui/button"
-import { LoginLink, RegisterLink } from "@kinde-oss/kinde-auth-nextjs/server"
+import { LoginLink, RegisterLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { ArrowRight } from "lucide-react"
+import UserAccountNav from "./UserAccountNav"
 
 const NavBar = () => {
+    const { getUser } = getKindeServerSession()
+    const user = getUser()
+
+
     return (
         <nav className="sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
             <MaxWidthWrapper>
@@ -16,7 +21,7 @@ const NavBar = () => {
                     {/* todo:add mobile nav-bar */}
 
                     <div className="hidden items-center space-x-4 sm:flex">
-                        <>
+                        {!user ? <>
                             <Link href="/pricing" className={buttonVariants({
                                 variant: "ghost",
                                 size: "sm"
@@ -28,7 +33,19 @@ const NavBar = () => {
                             <RegisterLink className={buttonVariants({
                                 size: "sm"
                             })}>Get Started <ArrowRight className="ml-1.5 h-5 w-5" /> </RegisterLink>
-                        </>
+                        </> : <>
+                            <Link href="/dashboard" className={buttonVariants({
+                                variant: "ghost",
+                                size: "sm"
+                            })}>Dashboard
+                            </Link>
+                            <UserAccountNav name={
+                                !user.given_name || !user.family_name ? "Your Account" : `${user.given_name} ${user.family_name}`
+                            } email={user.email ?? ''}
+                                imageUrl={user.picture ?? ''}
+                            />
+
+                        </>}
                     </div>
                 </div>
             </MaxWidthWrapper>
