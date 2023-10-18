829043e32791750390bf499329c8b4d63733b0ed
Vguetz <gomezlucas712@gmail.com>
2023-10-18 10:26:34 -0300

    fix:auth-callback

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,14 @@
-import Dashboard from "@/components/Dashboard";
-import { db } from "@/db";
-import { getUserSubscriptionPlan } from "@/lib/stripe";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { redirect } from "next/navigation";
+import Dashboard from '@/components/Dashboard'
+import { db } from '@/db'
+import { getUserSubscriptionPlan } from '@/lib/stripe'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
 
 const Page = async () => {
-    const { getUser } = getKindeServerSession();
-    const user = getUser();
+    const { getUser } = getKindeServerSession()
+    const user = getUser()
 
-    if (!user || !user.id) redirect('auth/callback?origin=dashboard')
+    if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
     const dbUser = await db.user.findFirst({
         where: {
@@ -16,7 +16,8 @@ const Page = async () => {
         }
     })
 
-    if (!dbUser) redirect('auth/callback?origin=dashboard')
+    if (!dbUser) redirect('/auth-callback?origin=dashboard')
+
     const subscriptionPlan = await getUserSubscriptionPlan()
 
     return <Dashboard subscriptionPlan={subscriptionPlan} />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <Link className={buttonVariants({
           size: 'lg',
           className: 'mt-5'
-        })} href="/sign-up" target="_blank">Get Started<ArrowRight className="ml-2 h-5 w-5" />
+        })} href="/dashboard" target="_blank">Get Started<ArrowRight className="ml-2 h-5 w-5" />
         </Link>
       </MaxWidthWrapper>
       {/*  value proposition section */}
